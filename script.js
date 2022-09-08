da7f5771f0f1a7ead4be154b3d5d0911ac12ebba
NadavArania <97458713+NadavArania@users.noreply.github.com>
2022-09-08 13:52:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// function FahernheitToCelsius(x){
+//     let fToC = (x - 32) * 5 / 9;
+//     document.getElementById("out1").innerHTML = fToC.toFixed(2);
+// }
+
+// function CelsiusToFahernheit(x){
+//     let cTof = x * 9 / 5 + 32;
+//     document.getElementById("out2").innerHTML = cTof.toFixed(2);
+// }
